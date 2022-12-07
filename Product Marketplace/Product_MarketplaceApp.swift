296522d9bcfadcc42a5b328cf4d6559b2653352d
Shahar Stahi <stahis@amazon.com>
2022-12-07 18:40:46 +0200

    Initial Commit

---
+++ Product Marketplace/Product_MarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Product_MarketplaceApp.swift
+//  Product Marketplace
+//
+//  Created by Stahi, Shahar on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Product_MarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
